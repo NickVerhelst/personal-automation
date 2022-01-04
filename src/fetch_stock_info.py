b4feeaad0419ec47fb44317298814f9aac190c07
Nick Verhelst <nick.verhelst@dexmach.com>
2022-01-04 08:59:23 +0100

    Added stock info retrieval automation.

---
+++ src/fetch_stock_info.py
@@ -0,0 +1,12 @@
+''' Live price of The Stock '''
+from yahoo_fin import stock_info
+live_price = stock_info.get_live_price("TSLA")
+print(round(live_price,2)," USD")
+
+
+''' Stock Price From 2019 to 2021 '''
+import yfinance as yf
+stockSymbol = 'TSLA'
+stockData = yf.Ticker(stockSymbol)
+stockDf_past_2 = stockData.history(period='5d', start='2019-1-1', end='2021-12-31')
+print(stockDf_past_2)
